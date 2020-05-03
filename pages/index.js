621850d06fbe42c32d5b10869cb471103811caa1
nanisampa <63031056+nanisampa@users.noreply.github.com>
2020-05-02 22:27:41 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ function Home(props) {
 }
 
 export async function getServerSideProps() {
-  const spreadSheetUrl = "https://docs.google.com/spreadsheets/d/12VdXr4m9QAQYw8Nxk31al6J4b-psI-8t/edit#gid=1831241289";
+  const spreadSheetUrl = "https://docs.google.com/spreadsheets/d/1eXwDV5PGImTNXOPcfkXKlPADJezEuSotNk8EkrkO2c4/edit#gid=1749062419";
   function getData() {
     return new Promise(resolve => {
       Tabletop.init({
