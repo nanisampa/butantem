b84f3d9a2821d1901cc9ad0702c8cac64b1e9b6c
nanisampa <63031056+nanisampa@users.noreply.github.com>
2020-05-04 10:09:49 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ function Home(props) {
   return (
     <Layout>
       <Head>
-        <title>MEX VS COVID-19 - Apoia as empresas e projetos brasileiros afetados pelo COVID-19</title>
+        <title>MEX VS COVID-19 - Apoia empresas e projetos do Butantã afetados pelo COVID-19</title>
         <meta
           key="description"
           name="description"
