df9b0d16b909d0ffd02675c7cceaf9e66aede601
nanisampa <63031056+nanisampa@users.noreply.github.com>
2020-05-06 20:26:52 -0300

    Add files via upload

--- pages/proyectos.js
+++ pages/proyectos.js
@@ -57,12 +57,12 @@ function Directorio(props) {
     const [deliveryFilter, setDeliveryFilter] = useState([
         {
             selected: true,
-            name: 'Sí',
+            name: 'Sim',
             id: 0
         },
         {
             selected: true,
-            name: 'No',
+            name: 'Não',
             id: 1
         }
     ]);
@@ -85,7 +85,7 @@ function Directorio(props) {
     return (
         <Layout>
             <Head>
-                <title>MEX VS COVID-19 - Conoce los proyectos que fueron afectados por el COVID-19 y cómo apoyarlos</title>
+                <title>Butantem Conheça os projetos que foram afetados pelo COVID-19 e como apoia-los</title>
                 <meta
                     key="description"
                     name="description"
@@ -96,7 +96,7 @@ function Directorio(props) {
             <main className="bg-covid-100">
                 <div className="container mx-auto ">
                     <div>
-                        <h2 className="text-2xl lg:text-3xl text-gray-900 leading-normal p-2 lg:p-10 text-center font-serif">Directorio de proyectos</h2>
+                        <h2 className="text-2xl lg:text-3xl text-gray-900 leading-normal p-2 lg:p-10 text-center font-serif">Diretório de Projetos</h2>
                     </div>
                     <TopSort numberOfListings={numberOfListings} />
                     <DirectoryList projects={projects} categories={props.categories} pagination={pagination} setPagination={setPagination} numberOfPages={numberOfPages} categoryFilter={categoryFilter} setCategoryFilter={setCategoryFilter} deliveryFilter={deliveryFilter} setDeliveryFilter={setDeliveryFilter} />
@@ -112,7 +112,7 @@ function Directorio(props) {
 }
 
 export async function getServerSideProps() {
-    const spreadSheetUrl = "https://docs.google.com/spreadsheets/d/1eXwDV5PGImTNXOPcfkXKlPADJezEuSotNk8EkrkO2c4/edit#gid=1749062419";
+    const spreadSheetUrl = "https://docs.google.com/spreadsheets/d/1Fh1oUfzVeQz2_2NMetk0nCXB7d45Vuf8ZJ-e9NGVcm8/edit?usp=sharing";
     function getData() {
         return new Promise(resolve => {
             Tabletop.init({
