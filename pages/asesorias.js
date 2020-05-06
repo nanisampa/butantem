5a6a1da6d40102d33c1692b2a1ac7d33cd793dde
nanisampa <63031056+nanisampa@users.noreply.github.com>
2020-05-06 11:44:00 -0300

    Update asesorias.js

--- pages/asesorias.js
+++ pages/asesorias.js
@@ -11,7 +11,7 @@ function Events(props) {
     return (
         <Layout>
             <Head>
-                <title>MEX VS COVID-19 - Especialistas para ajuda nos temas relacionados  COVID-19</title>
+                <title>Butanten-19 - Especialistas para ajuda nos temas relacionados  COVID-19</title>
                 <meta
                     key="description"
                     name="description"
