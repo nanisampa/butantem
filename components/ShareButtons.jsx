fdd6c467cb1189d3148160b8b5928491895a891e
nanisampa <63031056+nanisampa@users.noreply.github.com>
2020-05-06 19:43:36 -0300

    Add files via upload

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -13,11 +13,11 @@ import {
 
 function ShareButtons({ project }) {
     const router = useRouter()
-    const url = `https://mexvscovid19.com${router.asPath}`
-    const text = `Apoya a ${project.nombre} durante esta Pandemia. Conoce más sobre este y otros proyectos en MEX vs COVID-19.`
+    const url = `https://butantem.online/${router.asPath}`
+    const text = `Apoie ${project.nombre} durante esta Pandemia. Conheça mais sobre este e outros projetos do ButanTem.`
     return (
         <div className="flex flex-col">
-            <h4 className="text-xl font-medium text-gray-800 self-start font-serif mb-2">Compártelo</h4>
+            <h4 className="text-xl font-medium text-gray-800 self-start font-serif mb-2">Compartilhe</h4>
             <div className="flex flex-row justify-evenly">
                 <FacebookShareButton url={url} quote={text}>
                     <FacebookIcon size={32} round />
@@ -25,7 +25,7 @@ function ShareButtons({ project }) {
                 <WhatsappShareButton title={text} url={url}>
                     <WhatsappIcon size={32} round />
                 </WhatsappShareButton>
-                <TwitterShareButton title={text} url={url} related={['mexvscovid19']}>
+                <TwitterShareButton title={text} url={url} related={['butantem']}>
                     <TwitterIcon size={32} round />
                 </TwitterShareButton>
                 <LinkedinShareButton summary={text} url={url}>
